560b683c2be517cfc77fc546d411b2d611d77fd5
Kenneth Gollaher <kenneth.gollaher@gmail.com>
2022-08-13 23:28:05 -0400

    Updated

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ require('./app_api/config/passport');
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
+const roomsRouter = require("./app_server/routes/rooms");
+const newsRouter = require("./app_server/routes/news");
+const mealsRouter = require("./app_server/routes/meals");
+const contactRouter = require("./app_server/routes/contact");
+const aboutRouter = require("./app_server/routes/about");
 const apiRouter = require('./app_api/routes/index');
 
 const app = express();
@@ -42,10 +47,16 @@ app.use('/api', (req, res, next) => {
   next();
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/travel', travelRouter);
-app.use('/api', apiRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/travel", travelRouter);
+app.use("/rooms", roomsRouter);
+app.use("/news", newsRouter);
+app.use("/meals", mealsRouter);
+app.use("/contact", contactRouter);
+app.use("/about", aboutRouter);
+
+app.use("/api", apiRouter);
 
 // catch unauthorized error and create 401
 app.use((err, req, res, next) => {
